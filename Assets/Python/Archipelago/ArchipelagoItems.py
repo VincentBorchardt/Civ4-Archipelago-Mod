733093f7ebac6f79ff078e70b49389da92178a97
VincentBorchardt <vborchardt123@gmail.com>
2026-06-16 22:08:12 -0500

    Saving/loading archipelago items works
    BUG made this a lot easier than I thought it would be

--- Assets/Python/Archipelago/ArchipelagoItems.py
+++ Assets/Python/Archipelago/ArchipelagoItems.py
@@ -4,6 +4,7 @@ from PyHelpers import *
 from Popup import PyPopup
 
 import BugOptions
+import BugData
 
 import ArchipelagoStuff
 
@@ -19,8 +20,35 @@ TECH_TRANSLATION_DICT = {
     "Agriculture" : "TECH_AGRICULTURE"
 }
 
-# TODO Figure out a way to save this so you don't receive duplicates
+AP_DATA_KEY = "ArchipelagoMod"
+
 archipelagoReceivedItems = {}
+
+def saveArchipelagoData(*args):
+    """Saves the tracking dictionary into BUG's secure data registry."""
+    global archipelagoReceivedItems
+    try:
+        # Get the global BUG data registry
+        data = BugData.getGameData()
+        # Store your dictionary inside your unique namespace slot
+        data[AP_DATA_KEY] = archipelagoReceivedItems
+    except Exception, e:
+        CyInterface().addImmediateMessage("AP Save Error: " + str(e), "")
+
+def loadArchipelagoData(*args):
+    """Loads the tracking dictionary from BUG's data registry."""
+    global archipelagoReceivedItems
+    try:
+        data = BugData.getGameData()
+        # Retrieve your dictionary, defaulting to an empty dict if it doesn't exist yet
+        if AP_DATA_KEY in data:
+            archipelagoReceivedItems = data[AP_DATA_KEY]
+        else:
+            archipelagoReceivedItems = {}
+    except Exception, e:
+        CyInterface().addImmediateMessage("AP Load Error: " + str(e), "")
+        archipelagoReceivedItems = {}
+
 
 def receiveItems():
     messageDict = {"type":"ReceiveItems"}
@@ -43,6 +71,7 @@ def receiveItems():
                 grantTech(item_name)
                 CyInterface().addImmediateMessage("Received " + item_name + " from " + player_name, "")
             archipelagoReceivedItems[item_index] = item_name
+            saveArchipelagoData()
     else:
         ArchipelagoStuff.showPopup("Connection Error", "Unexpected packet type: " + dataDict.get("cmd"))
 
